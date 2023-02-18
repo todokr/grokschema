016e6fa596f25b32a57f6435fb7f8b00375de925
shunsuke.tadokoro <shunsuke.tadokoro@bizreach.co.jp>
2023-02-18 17:45:28 +0900

    Config

--- src/main/scala/schemareader/Config.scala
+++ src/main/scala/schemareader/Config.scala
@@ -1,7 +1,37 @@
 package schemareader
 
-trait ConfigLoader:
-  def load() = ???
+import java.nio.file.Path
+import java.nio.charset.StandardCharsets
+
+import scala.io.Source
+import java.util.Properties
+
+object JdbcConfig:
+  def loadFromProperties(path: Path): JdbcConfig =
+    val prop = new Properties()
+    val reader = Source.fromFile(path.toFile, StandardCharsets.UTF_8.name).bufferedReader
+    prop.load(reader)
+    
+    val driver   = prop.getString(DriverKey)
+    val url      = prop.getString(UrlKey)
+    val user     = prop.getString(UserKey)
+    val password = prop.getString(PasswordKey)
+    val schema   = prop.getStringOr(SchemaKey, DefaultSchema)
+    JdbcConfig(driver, url, user, password, schema)
+  
+  private val DriverKey   = "db.driver"
+  private val UrlKey      = "db.url"
+  private val UserKey     = "db.user"
+  private val PasswordKey = "db.password"
+  private val SchemaKey   = "db.schema"
+  private val DefaultSchema = "public"
+
+  extension (p: Properties)
+    def getString(key: String): String =
+      Option(p.getProperty(key)).getOrElse(throw new JdbcConfigException(key))
+
+    def getStringOr(key: String, default: String): String =
+      Option(p.getProperty(key)).getOrElse(default)
 
 case class JdbcConfig(
     driver: String,
@@ -15,3 +45,6 @@ case class JdbcConfig(
        |url=$url
        |user=$user
        |password=${password.map(_ => "*").mkString}""".stripMargin
+
+class JdbcConfigException(key: String) extends Exception(s"$key is not set")
+

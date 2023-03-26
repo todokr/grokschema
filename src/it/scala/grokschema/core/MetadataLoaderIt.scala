85e1fb0b949a11afcbebbce46fbb4216b07f02a8
todokr <s.tadokoro0317@gmail.com>
2023-03-27 01:46:11 +0900

    add integration test

--- src/it/scala/grokschema/core/MetadataLoaderIt.scala
+++ src/it/scala/grokschema/core/MetadataLoaderIt.scala
@@ -12,7 +12,57 @@ class MetadataLoaderIt extends FunSuite:
   )
   val loader = MetadataLoader(config)
 
-  test("hoge") {
-    val refs = loader.loadReferences()
-    refs.foreach(println)
+  test("MetadataLoader#loadReferences") {
+    val actual = loader.loadReferences()
+    val expected = Set(
+      Reference(
+        "c_id-->id",
+        TableId("public", "a"),
+        "c_id",
+        TableId("public", "c"),
+        "id"
+      ),
+      Reference(
+        "d_id-->id",
+        TableId("public", "b"),
+        "d_id",
+        TableId("public", "d"),
+        "id"
+      ),
+      Reference(
+        "e_id-->id",
+        TableId("public", "c"),
+        "e_id",
+        TableId("public", "e"),
+        "id"
+      ),
+      Reference(
+        "f_id-->id",
+        TableId("public", "d"),
+        "f_id",
+        TableId("public", "f"),
+        "id"
+      ),
+      Reference(
+        "y_id-->id",
+        TableId("public", "a"),
+        "y_id",
+        TableId("public", "y"),
+        "id"
+      ),
+      Reference(
+        "x_id-->id",
+        TableId("public", "b"),
+        "x_id",
+        TableId("public", "x"),
+        "id"
+      ),
+      Reference(
+        "x_id-->id",
+        TableId("public", "y"),
+        "x_id",
+        TableId("public", "x"),
+        "id"
+      )
+    )
   }
